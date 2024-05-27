a77feca21d456eaba5e3ba96a39468af92a1167e
aniketkumar7 <aniketkumar639597@gmail.com>
2024-05-27 11:36:40 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-import os
 from groq import Groq
-from dotenv import load_dotenv
 
-load_dotenv()
 
-client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
-)
+def get_api_key():
+    """
+    Prompts the user to enter their GROQ API key.
+
+    Returns:
+        str: The user-provided API key.
+    """
+    api_key = input("Please enter your GROQ API key: ")
+    return api_key
 
 
 def chat_with_ai():
+    """
+    Initiates the chat session with the AI tutor.
+    """
     print("Welcome to your personal AI tutor Type your questions and I'll do my best to help.")
     print("Type 'exit' to quit.")
-    
+
+    api_key = get_api_key()
+    client = Groq(api_key=api_key,)
 
     while True:
         user_input = input("You: ")
@@ -20,6 +28,7 @@ def chat_with_ai():
             print("See you next time GoodDay! \n")
             break
 
+        # Send the user input to the AI tutor
         chat_completion = client.chat.completions.create(
             messages=[
                 {
@@ -32,12 +41,14 @@ def chat_with_ai():
             max_tokens=450,
             top_p=0.9,
             stream=False,
-            stop="exit",            
+            stop="exit",
         )
 
-        response = chat_completion.choices[0].message.content        
+        # Print the AI tutor's response
+        response = chat_completion.choices[0].message.content
         print("AI Tutor: \n", response)
 
 
+# Start the chat session
 if __name__ == "__main__":
     chat_with_ai()
